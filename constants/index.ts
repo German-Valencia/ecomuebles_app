6395759695793aebe854514a4758d19f0a67db71
Germán Alberto Valencia Torres <lenovopromotor@gmail.com>
2023-11-12 12:41:55 -0500

    commit linux 2

--- constants/index.ts
+++ constants/index.ts
@@ -1,10 +1,10 @@
 // NAVIGATION
 export const NAV_LINKS = [
-    { href: '/', key: 'home', label: 'Home' },
-    { href: '/', key: 'how_hilink_work', label: 'How Hilink Work?' },
-    { href: '/', key: 'services', label: 'Services' },
-    { href: '/', key: 'pricing ', label: 'Pricing ' },
-    { href: '/', key: 'contact_us', label: 'Contact Us' },
+    { href: '/', key: 'home', label: 'Inicio' },
+    { href: '/', key: 'how_hilink_work', label: 'Funcionamiento' },
+    { href: '/', key: 'services', label: 'Servicios' },
+    { href: '/', key: 'pricing ', label: 'Precios' },
+    { href: '/', key: 'contact_us', label: 'Contáctanos' },
   ];
   
   // CAMP SECTION
