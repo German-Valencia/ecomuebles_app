9f1494c92803fb913482db48d1fecdf3c1d35081
Germán Alberto Valencia Torres <lenovopromotor@gmail.com>
2023-11-15 18:41:10 -0500

    detalles error key ok

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
           </Link>
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
             {FOOTER_LINKS.map((columns) => (
-              <FooterColumn title={columns.title}>
+              <FooterColumn title={columns.title} key={columns.title}>
                 <ul className="regular-14 flex flex-col gap-4 text-gray-30">
                   {columns.links.map((link) => (
                     <Link href="/" key={link}>
@@ -60,12 +60,12 @@ const Footer = () => {
   );
 };
 
-type FooterColumnsProps = {
+type FooterColumnProps = {
   title: string;
   children: React.ReactNode;
 };
 
-const FooterColumn = ({ title, children }: FooterColumnsProps) => {
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
   return (
     <div className="flex flex-col gap-5">
       <h4 className="bold-18 whitespace-nowrap">{title}</h4>
